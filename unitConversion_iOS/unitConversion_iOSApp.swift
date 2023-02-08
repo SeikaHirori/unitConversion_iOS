c6619f42aa948d584f13490ef0f65868701dcede
SeikaHirori <SeikaHirori@outlook.com>
2023-02-08 10:30:16 -0800

    Initial Commit

---
+++ unitConversion_iOS/unitConversion_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitConversion_iOSApp.swift
+//  unitConversion_iOS
+//
+//  Created by Seika Hirori on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct unitConversion_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ unitConversion_iOSUITests/unitConversion_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  unitConversion_iOSUITestsLaunchTests.swift
+//  unitConversion_iOSUITests
+//
+//  Created by Seika Hirori on 2/8/23.
+//
+
+import XCTest
+
+final class unitConversion_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
